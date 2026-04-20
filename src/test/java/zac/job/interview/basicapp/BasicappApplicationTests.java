79986f88c3cade39efc4306de8bb0cf764e9f6ae
Zach <zach@Zachs-Mac-mini.local>
2026-04-20 15:22:46 -0500

    init starter

---
+++ src/test/java/zac/job/interview/basicapp/BasicappApplicationTests.java
@@ -0,0 +1,13 @@
+package zac.job.interview.basicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
